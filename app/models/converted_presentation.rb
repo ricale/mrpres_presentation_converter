6bdc1ccc13cc2bff744038a39fc92710be93c239
KIM Kangseong <kangseong@adfresca.com>
2014-06-02 21:57:39 +0900

    Update converted_presentation

--- app/models/converted_presentation.rb
+++ app/models/converted_presentation.rb
@@ -20,11 +20,32 @@ class ConvertedPresentation < ActiveRecord::Base
 
   before_validation :default_values
 
+  after_validation :local_timezone_on_create, on: :create
+  after_validation :local_timezone_on_update, on: :update
+
+  after_save :default_timezone
+
   CONVERTING = 0
   COMPLETE   = 1
   FAILED     = 2
 
   def default_values
     self.status ||= ConvertedPresentation::CONVERTING
+  end
+
+  def local_timezone_on_create
+    ConvertedPresentation.record_timestamps = false
+    self.created_at = Time.now.utc + 9.hours
+    puts self.created_at
+  end
+
+  def local_timezone_on_update
+    ConvertedPresentation.record_timestamps = false
+    self.updated_at = Time.now.utc + 9.hours
+    puts self.updated_at
+  end
+
+  def default_timezone
+    ConvertedPresentation.record_timestamps = true
   end
 end
